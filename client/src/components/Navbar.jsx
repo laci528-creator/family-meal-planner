7664c07d0abbbcfb3cf2d4d985056afa728664a3
Laszlo Haraszti <laci528@gmail.com>
2026-09-15 21:03:11 +0200

    feat: add navbar navigation links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import { NavLink } from 'react-router-dom';
+
 function Navbar() {
+
+
   return (
-    <nav>
-      <h2>Family Meal Planner</h2>
+    <nav className="navbar">
+      <div className="navbar-content">
+        <div className="nav-links">
+            <NavLink to="/" className={({ isActive }) => `nav-link-a${isActive ? " active" : ""}`} >Home</NavLink>
+            <NavLink to="/recipes" className={({ isActive }) => `nav-link-a${isActive ? " active" : ""}`}>Recipes</NavLink>
+            <NavLink to="/planner" className={({ isActive }) => `nav-link-a${isActive ? " active" : ""}`}>Planner</NavLink>
+            <NavLink to="/shopping" className={({ isActive }) => `nav-link-a${isActive ? " active" : ""}`}>Shopping</NavLink>
+            <NavLink to="/login" className={({ isActive }) => `nav-link-a${isActive ? " active" : ""}`}>Login</NavLink>
+            <NavLink to="/register" className={({ isActive }) => `nav-link-a${isActive ? " active" : ""}`}>Register</NavLink>
+        </div>
+      </div>
     </nav>
   );
 }
